f472a0447ed7043d8f178d4ba8f6e3abb97d9f82
Bruno Dzogovic <bruno@oslomet.no>
2022-01-12 13:59:30 +0100

    added manual build for the uhd drivers

--- env.sh
+++ env.sh
@@ -8,7 +8,7 @@ git pull
 file="/openairinterface5g/targets/ARCH/USRP/USERSPACE/LIB/usrp_lib.cpp"
 sed -i "s/122.88e6/$CLOCK_RATE\e6/g" "$file"
 cd /openairinterface5g/cmake_targets/
-./build_oai -i -I -w USRP --gNB --build-lib "nrscope" 
+./build_oai -i -I --gNB --build-lib "nrscope" 
 process_id=$!
 wait $process_id 
 exit 
